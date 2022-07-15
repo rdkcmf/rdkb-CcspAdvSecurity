8045e3315b27fdbad9d47edf0e391fb3c12d9898
Gujulva Jagadeesh, Santhosh <Santhosh_GujulvaJagadeesh@comcast.com>
2022-07-15 07:25:47 +0000

    RDKB-43299 : Add subscribe/callback to report nflua_mem
    Reason for change: NFLua should report to cujo-agent process when
                       nflua_mem exceeds threshold.
    Test Procedure: NFLua memory info is sent from nflua to cujo-agent
                    periodically and cujo-agent exits once nflua_mem
                    reaches the threshold.
    Risks: Low
    Signed-off-by: Santhosh_GujulvaJagadeesh@comcast.com
    
    (cherry picked from commit b4bd439ca950ae21decb22cda5ceb7d5ca4afd2b)
    Change-Id: I5bbd8bafba2f1bd5bc7849437752e6230ea0cf92

--- scripts/advsec_cpu_mem_recovery.sh
+++ scripts/advsec_cpu_mem_recovery.sh
@@ -130,20 +130,6 @@ log_agent_mem_statistics()
 			exit
 		fi
 	fi
-
-	${RUNTIME_DIR}/bin/${CUJO_AGENT_SH} -e 'cujo.nf.dostring([[print("nfluamem:"..collectgarbage("count"))]])'
-	nflua_rss=`dmesg | grep nfluamem: | tail -1 | cut -d':' -f2`
-	if [ "${nflua_rss}" = "" ]; then
-		nflua_rss=0
-	fi
-	# nflua_rss is in bytes
-	nflua_rss=$((${nflua_rss} / $KB))
-	echo_t "NFLua memory usage:${nflua_rss}" >> $ADVSEC_AGENT_LOG_PATH
-
-	if [ "${nflua_rss}" -ge "${MAX_RSS_THRESHOLD}" ]; then
-		advsec_restart_agent "NfluaHighRSS"
-		exit
-	fi
 }
 
 get_agent_pid_list
